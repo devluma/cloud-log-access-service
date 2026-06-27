77c0b5efca64cd5cb2b3592f0329a1ed757c3ff2
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2026-06-27 01:26:35 -0300

    chore: add application logger service

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,17 +1,21 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { LogsModule } from './logs/logs.module';
-import { AwsModule } from './aws/aws.module';
+import { LoggerModule } from './common/logger/logger.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [configuration],
     }),
+
+    LoggerModule,
+
     AuthModule,
     LogsModule,
     AwsModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
